360a89e008f35606ae764e96e8f9059907404120
Natsuha <ethanyap8@gmail.com>
2025-11-27 14:56:31 +0800

    fix package

--- com.github.natsuhakoishi.avajlauncher/Exception/InvalidFileContentException.java
+++
@@ -1,9 +0,0 @@
-package com.github.natsuhakoishi.avajlauncher.Exception;
-
-public class InvalidFileContentException extends Exception
-{
-	public InvalidFileContentException(String msg)
-	{
-		super("InvalidFileContentException: "+msg);
-	}
-}
