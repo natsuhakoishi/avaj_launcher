53510c5e3444afe9018869bdac2d842aef9baa93
Natsuha <ethanyap8@gmail.com>
2025-11-27 14:51:26 +0800

    remove unnecessary things

--- srcs/Exception/InvalidFileContentException.java
+++
@@ -1,9 +0,0 @@
-package srcs.Exception;
-
-public class InvalidFileContentException extends Exception
-{
-	public InvalidFileContentException(String msg)
-	{
-		super("InvalidFileContentException: "+msg);
-	}
-}
